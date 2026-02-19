bf5952bed95f4754229395560e263466b39a4163
mojo0206 <brojashree73@gmail.com>
2026-02-19 10:55:06 +0530

    UC5:Hardcoded palindrome

--- src/PalindromeCheckerApp1.java
+++ src/PalindromeCheckerApp1.java
@@ -1,37 +1,27 @@
+import java.util.Stack;
 public class PalindromeCheckerApp1 {
     public static void main(String[] args){
-        //UC4
-        // Declare and initialize hte input string
-        String input = "radar";
+        String input = "noon";
 
-        // Convert the string into a character array
-        char[] chars = input.toCharArray();
+        //Create a Stack to store characters.
+        Stack<Character> stack = new Stack<>();
 
-        // Initialize pointer at the beginning
-        int start = 0;
-
-        // initialize pointer at the end
-        int end = chars.length - 1;
-
-        // Assume palindrome initially
-        boolean isPalindrome = true;
-
-        // Continue comparison until pointers cross.
-        while (start < end){
-
-            if (chars[start] != chars[end]){
-                isPalindrome = false;
-                break;
-
-            }
-
-            start++; //move forward
-            end--; //move backward
-
+        // Push each character of the string into the stack.
+        for (char c : input.toCharArray()){
+            stack.push(c);
         }
 
-        System.out.println("Is it a Palindrome? : " + isPalindrome);
+        // Assume palindrome initially.
+        boolean isPalindrome = true;
 
+        // Iterate again through original string
+        for (char c : input.toCharArray()) {
+            if (c != stack.pop()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+        System.out.println("Is it a palindrome? : " + isPalindrome);
 
     }
 }
