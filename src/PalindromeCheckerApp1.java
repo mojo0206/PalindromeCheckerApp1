7589c76d7f63ee1b974a7727223dbd1655bbf8c8
mojo0206 <brojashree73@gmail.com>
2026-02-19 10:28:14 +0530

    UC3:Hardcoded palindrome

--- src/PalindromeCheckerApp1.java
+++ src/PalindromeCheckerApp1.java
@@ -1,21 +1,30 @@
 public class PalindromeCheckerApp1 {
     public static void main(String[] args){
+        System.out.println("Welcome to the Palindrome Checker app !");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialized successfully");
+
         String input = "madam";
-
         boolean isPalindrome = true;
+        System.out.println("Input text: " + input);
 
-        for(int i=0; i<input.length()/2;i++){
-
-            if(input.charAt(i) != input.charAt(input.length() - 1 - i)){
+        int length = input.length();
+        for (int i = 0; i < input.length() / 2; i++){
+            if (input.charAt(i) != input.charAt(length - 1 - i)){
                 isPalindrome = false;
                 break;
             }
         }
+        System.out.println("Is it a Palindrome? :" + isPalindrome);
 
-        if(isPalindrome){
-            System.out.println(input+" is a palindrome.");
-        }else{
-            System.out.println(input+" is not a palindrome.");
+        StringBuilder reversed = new StringBuilder();
+        for(int i = input.length() - 1; i >= 0; i--){
+            reversed.append(input.charAt(i));
         }
+
+        System.out.println("original: " + input);
+        System.out.println("Reversed: " + reversed);
+
+
     }
 }
