ca3e5652cc237ef5c55bd3c66db4deadaacedc74
mojo0206 <brojashree73@gmail.com>
2026-02-26 09:17:46 +0530

    UC10:Hardcoded palindrome

--- src/PalindromeCheckerApp1.java
+++ src/PalindromeCheckerApp1.java
@@ -1,71 +1,27 @@
-/**
- * =========================================================
- * MAIN CLASS - UseCase9PalindromeCheckerApp
- * =========================================================
- *
- * Use Case 9 : Recursive Palindrome Checker
- *
- * Description:
- * This class validates a palindrome using recursion.
- *
- * Characters are compared from the outer positions
- * moving inward using recursive calls.
- *
- * The recursion stops when:
- * - All characters are matched, or
- * - A mismatch is found.
- *
- * This use case demonstrates divide-and-conquer
- * logic using method recursion.
- *
- * @author Developer
- * @version 1.0
- */
+//USE CASE 10
 
 import java.util.Scanner;
 
 public class PalindromeCheckerApp1{
 
-    /**
-     * Application entry point for UC9.
-     *
-     * @param args Command-line arguments
-     */
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Input : ");
+        System.out.print("Input: ");
         String input = scanner.nextLine();
 
-        boolean result = check(input, 0, input.length() - 1);
+        String normalized = input.replaceAll("\\s+","").toLowerCase();
 
-        System.out.println("Is Palindrome? : " + result);
+        boolean isPalindrome = true;
 
+        for(int i=0; i<normalized.length()/2;i++){
+            if(normalized.charAt(i)!=normalized.charAt(normalized.length()-1-i)){
+                isPalindrome = false;
+                break;
+            }
+        }
+        System.out.println("Is Palindrome?: "+isPalindrome);
         scanner.close();
-    }
-
-    /**
-     * Recursively checks whether a string is palindrome.
-     *
-     * @param s     Input string
-     * @param start Starting index
-     * @param end   Ending index
-     * @return true if palindrome, otherwise false
-     */
-    private static boolean check(String s, int start, int end) {
-
-        // Base Condition 1: All characters checked
-        if (start >= end) {
-            return true;
-        }
-
-        // Base Condition 2: Mismatch found
-        if (s.charAt(start) != s.charAt(end)) {
-            return false;
-        }
-
-        // Recursive call (move inward)
-        return check(s, start + 1, end - 1);
     }
 }
